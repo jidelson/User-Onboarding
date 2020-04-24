95d23dd764907f57df9d05cac0d9f3db67d2e846
jidelson <58210030+jidelson@users.noreply.github.com>
2020-04-23 20:18:56 -0700

    got validation working, now need to fix submit button

--- joes/src/App.js
+++ joes/src/App.js
@@ -6,6 +6,10 @@ import User from './User.js'
 import axios from 'axios';
 import * as yup from "yup";
 
+//THIS IS THE URL FOR OUT GET AND POST REQUESTS
+const url= 'https://reqres.in/api/users'
+
+//SHAPE THAT DRIVES STATE OF FORM
 const initialFormValues = [
   {
     name:'',
@@ -16,14 +20,15 @@ const initialFormValues = [
 }
 ];
 
+//SHAPE OF VALIDATION ERRORS OBJECT
 const initialFormErrors = {
 name:'',
 email:'',
 password: '',
 civil:'',
-termsOfService:''
 }
 
+//BUILD A SCHEMA FOR VALIDATION 
 const formSchema = yup.object().shape({
   name: yup
   .string()
@@ -43,25 +48,31 @@ const formSchema = yup.object().shape({
   password: yup
   .string()
   .min(6, 'Password must have at least 6 characters')
-  .required('Password is required')
+  .required('Password is required'),
   
-  // termsOfService:
+  termsOfService: yup
+  .boolean()
+  .oneOf ([true], "You must accept Terms Of Service")
 })
 
 
 
-const url= 'https://reqres.in/api/users'
 
 
 
 
+//********YOUR ACTUAL APPLICATION******** */
 function App() {
 
 const [user, setUser] = useState([]) 
 const [formValues, setFormValues] = useState(initialFormValues)
+
+//STATE KEEPS TRACK OF IF SUBMIT BUTTON IS DISABLED OR NOT
 const [formDisabled, setFormDisabled] = useState(true)
+//STATE NEEDS TO KEEP TRACK OF VALIDATION ERRORS
 const [formErrors, setFormErrors] = useState(initialFormErrors)
 
+// FETCH YOUR USERS FROM API AND SET THEM IN STATE
 const getUsers = () => {
   axios.get(url)
   .then(res => {
@@ -71,10 +82,13 @@ const getUsers = () => {
   })
 }
 
+//AFTER FIRST "DOM SURGERY" WE NEED TO GET OUR USERS FROM API
 useEffect(() =>{
 getUsers()
 }, [])
 
+// WE NEED A FUNCTION TO ADD A NEW USER TO API
+// AND SET AN UPDATED LIST OF USERS IN STATE
 const postUser = user => {
   axios.get(url, user)
   .then(res => {
@@ -82,6 +96,34 @@ const postUser = user => {
   })
   .catch(err => {
   })
+}
+
+// IF FORM IS CHANGED WE NEED TO RUN VALIDATION
+// AND USE CHANGES TO ENABLE/DISABLE SUBMIT BUTTON
+useEffect(() => {
+  formSchema.isValid(formValues)
+    .then(valid => { 
+      setFormDisabled(!valid)
+    })
+}, [setFormValues])
+
+const onSubmit = evt => {
+  evt.preventDefault()
+
+
+    const newUser = {
+    name: formValues.name,
+    email: formValues.email,
+    civil: formValues.civil === 'single' ? false : true,
+    password: formValues.password,
+    termsOfService: Object.keys(formValues.termsOfService)
+    .filter(termsOfService => formValues.termsOfService[termsOfService] ===true)
+    }
+
+
+    // setUser([...user, newUser])
+    postUser(newUser)
+    setFormValues(initialFormValues)
 }
 
 const onInputChange = evt => {
@@ -114,42 +156,13 @@ const onCheckBoxChange = evt => {
   evt.preventDefault()
 }
 
-useEffect(() => {
-  formSchema.isValid(formValues)
-    .then(valid => { 
-      setFormDisabled(!valid)
-    })
-}, [setFormValues])
-
-const onSubmit = evt => {
-  evt.preventDefault()
-
-    const newUser = {
-    name: formValues.name,
-    email: formValues.email,
-    civil: formValues.civil === 'single' ? false : true,
-    password: formValues.password,
-    termsOfService: Object.keys(formValues.termsOfService)
-    .filter(termsOfService => formValues.termsOfService[termsOfService] ===true)
-    }
-    setUser([...user, newUser])
-    postUser(newUser)
-    setFormValues(initialFormValues)
-}
-
   return (
     <div className="App">
       <header className="App-header">
       <h1>User Form</h1>
       </header>
 
-      {/* {
-        user.map(user => {
-          return (  
-            <User key={user.id} details={user} />
-          )
-        })
-      } */}
+     
 
       <Form
       values = {formValues}
@@ -159,6 +172,14 @@ const onSubmit = evt => {
       disabled ={formDisabled}
       errors ={formErrors}
       />
+   
+    {/* {
+        user.map(user => {
+          return (  
+            <User key={user.id} details={user} />
+          )
+        })
+      } */}
     </div>
   );
 };
--- joes/src/Form.js
+++ joes/src/Form.js
@@ -2,6 +2,7 @@ import React from 'react';
 import './App.css';
 
 
+
 function Form(props){
     const {
         values,
@@ -11,46 +12,46 @@ function Form(props){
         disabled,
         errors
     } = props
+
 return(
 <form className='user container'>
+    <h2>The User Form</h2>
     <div className = 'errors'>
-
+         {errors.name}
+        {errors.email}
+        {errors.civil}
+        {errors.password}
     </div>
 
-    <label>
-        Name:&nbsp;
+    <label>Name:&nbsp;
         <input
         value = {values.name}
         onChange = {onInputChange}
         name='name'
         type='text'
-        />
-    </label>
+        /></label>
 
-    <label>
-        Email:&nbsp;
+    <label>Email:&nbsp;
                <input 
                 value = {values.email}
                 onChange = {onInputChange}
                name='email'
                type='text' 
-               />
-            </label>
+               /></label>
 
-            <label>
-        Password:&nbsp;
+            <label> Password:&nbsp;
                <input 
                 value = {values.password}
                 onChange = {onInputChange}
                name='password'
                type='password' 
-               placeholder="password"
-               />
-            </label>
+            //    placeholder="password"
+               /></label>
 
+           {/* ****DROPDOWN***** */}
             <label>Civil Status:&nbsp;
       <select
-          value ={values.onCheckboxChange}
+          value ={values.civil}
           onChange = {onInputChange}
           name='civil'
         >
@@ -59,18 +60,19 @@ return(
           <option value='single'>not married</option>
         </select></label>
 
-
+{/* ****CHECKBOX***** */}
 <label>
     <input
     name='termsOfService'
     type="checkbox"
+    checked={values.termsOfService}
+    onChange={onCheckBoxChange}
     />
-    Terms Of Service
+    Terms Of Service</label>
 
-</label>
 
     <div>
-    <button onClick={onSubmit} disabled={disabled}>Submit</button>
+    <button onClick={onSubmit} disabled={disabled} >Submit</button>
     </div>
 
 </form>
